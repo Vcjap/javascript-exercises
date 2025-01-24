e498ba7e630d85f40e56e49c83fb2aa1865214ef
Vcjap <30173302+Vcjap@users.noreply.github.com>
2025-01-24 17:47:45 +0100

    Refactor function to pass all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
 const sumAll = function(n1,n2) {
+    //Get min and max 
     const minNum = Math.min(n1,n2)
     const maxNum = Math.max(n1,n2)
+
+    //Check other conditions
+    isNumNegative = minNum < 0;
+    // Need to check n1 and n2 because Math.min and Math.max turn strings into numbers
+    isNumNotAnInteger = !(Number.isInteger(n1) && Number.isInteger(n2)); 
+    if (isNumNegative || isNumNotAnInteger) {
+        return "ERROR";
+    }
+    // Create Sum
     let sumNum = 0
     for (lastNum = minNum; lastNum <= maxNum; lastNum++) {
         sumNum += lastNum
