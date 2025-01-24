b46b6a0c13973cc71e8619093beca550b1a8d904
Vcjap <30173302+Vcjap@users.noreply.github.com>
2025-01-24 17:29:22 +0100

    Create function to pass 1st test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(n1,n2) {
+    const minNum = Math.min(n1,n2)
+    const maxNum = Math.max(n1,n2)
+    let sumNum = 0
+    for (lastNum = minNum; lastNum <= maxNum; lastNum++) {
+        sumNum += lastNum
+    }
+    return sumNum
 };
 
 // Do not edit below this line
