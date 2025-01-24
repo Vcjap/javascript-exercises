3b322f4e450ff53a86de4a2f172b46ab922ba9bb
Vcjap <30173302+Vcjap@users.noreply.github.com>
2025-01-24 16:10:27 +0100

    Write reverseString function to pass all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(stringToReverse) {
+    let stringReversed = []
+    for (const letter of stringToReverse) {
+        stringReversed.unshift(letter)
+    }
+    return stringReversed.join("")
 };
 
 // Do not edit below this line
