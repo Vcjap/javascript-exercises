980ed5c0c7c7c6747662ac172d82ac4d35a80bc1
Vcjap <30173302+Vcjap@users.noreply.github.com>
2025-01-24 16:00:05 +0100

    Modify repeatString to return ERROR for negative numnbers

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,6 @@
 const repeatString = function(initialString, times) {
     let finalString = ""
+    times < 0 ? finalString += "ERROR" : finalString = ""; // if times < 0, return an error. The loop won't be run.
     for (i = 0; i < times; i++) {
         finalString += initialString
     }
