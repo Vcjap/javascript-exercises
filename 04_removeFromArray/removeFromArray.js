e8f5bb896e1a294f3711e558a786c7a08dfa1ec9
Vcjap <30173302+Vcjap@users.noreply.github.com>
2025-01-24 16:22:22 +0100

    Create function to pass first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(incomingArray, valueToRemove) {
+    const indexValuetoRemove = incomingArray.indexOf(valueToRemove);
+    const removedValues = incomingArray.splice(indexValuetoRemove,1);
+    return incomingArray
 };
 
 // Do not edit below this line
