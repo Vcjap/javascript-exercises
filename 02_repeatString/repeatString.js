74c5c417fa3b840fbda088f849e26f75b00b9088
Vcjap <30173302+Vcjap@users.noreply.github.com>
2025-01-24 15:53:20 +0100

    Create function to complete first test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,9 @@
-const repeatString = function(string, times) {
-    
-
+const repeatString = function(initialString, times) {
+    let finalString = ""
+    for (i = 0; i < times; i++) {
+        finalString += initialString
+    }
+    return finalString
 };
 
 // Do not edit below this line
