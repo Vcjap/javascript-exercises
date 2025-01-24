f3e14aef4e695fe5641c7d20ceb83d2a2a9ec4f7
Vcjap <30173302+Vcjap@users.noreply.github.com>
2025-01-24 18:11:22 +0100

    Write function tempConversion to pass all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureFar) {
+  const temperatureCel = (temperatureFar - 32)*5/9;
+  return Math.round(temperatureCel * 10) / 10; // return the temperature with one decimal place
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureCel) {
+  const temperatureFar = temperatureCel*9/5 + 32;
+  return Math.round(temperatureFar * 10) / 10;
 };
 
 // Do not edit below this line
